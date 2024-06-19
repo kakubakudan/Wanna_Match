5b3299086fbe4a2f8f6b068948e1a261d26128ae
かくばくだん <kakubakudan@kakubakudannoMacBook-Air.local>
2024-06-19 09:07:56 +0900

    Fix issues and update HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+// src/screens/HomeScreen.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Wanna_Match!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
