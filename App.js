6d46b47d070bcd11b8abe2b3a98eff092363f2f1
kakubakudan <soto.1101.minipad@gmail.com>
2024-06-26 16:14:02 +0900

    Updated App.js with navigation and gesture handler setup

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import * as React from 'react';
+import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import HomeScreen from './src/screens/HomeScreen';
+import MatchScreen from './src/screens/MatchScreen';
+import MatchHistoryScreen from './src/screens/MatchHistoryScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Match" component={MatchScreen} />
+          <Stack.Screen name="MatchHistory" component={MatchHistoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
+
 
