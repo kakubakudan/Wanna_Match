7673de1d84ff3520c5d63d2deb97c5ff709dc577
kakubakudan <soto.1101.minipad@gmail.com>
2024-07-11 19:46:05 +0900

    Added bottom tab navigation and adjusted HomeScreen layout

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
-import MatchHistoryScreen from './src/screens/MatchHistoryScreen';
 import MatchScreen from './src/screens/MatchScreen';
+import MatchHistoryScreen from './src/screens/MatchHistoryScreen';
+import ProfileScreen from './src/screens/ProfileScreen'; // プロファイル画面も追加
+
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Match" component={MatchScreen} />
+      <Stack.Screen name="MatchHistory" component={MatchHistoryScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ headerShown: false }} // ヘッダーを非表示にする
-        />
-        <Stack.Screen name="MatchHistory" component={MatchHistoryScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
